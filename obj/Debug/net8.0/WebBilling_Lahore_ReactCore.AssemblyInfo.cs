18319c066137e8a20e4bf471eaacd7dc3a963e08
muhammadqasim278 <qasimmuhammad278@gmail.com>
2026-07-01 11:59:43 +0500

    OK

--- obj/Debug/net8.0/WebBilling_Lahore_ReactCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebBilling_Lahore_ReactCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBilling_Lahore_ReactCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852f87ab01525f5b8bea24d6b2f75f044277163b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8fd9a4199a8614ba23e0d1ff48db7b1959dcdac")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBilling_Lahore_ReactCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBilling_Lahore_ReactCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
